58512f59c7183974a4ffe05fdcecefc1def2c77b
chstrcpprpt <cjhallen@gmail.com>
2019-12-22 17:14:47 +1100

    added puppeteer and write file function

--- generateHTML.js
+++ generateHTML.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 const ejs = require("ejs");
+const puppeteer = require('puppeteer');
 
-const renderHTML = function(username, favColour) {
+const renderHTML = async function(username, favColour, data) {
   const template = fs.readFileSync("./template.ejs", "utf-8");
 
   const htmlString = ejs.render(template, 
@@ -12,7 +13,22 @@ const renderHTML = function(username, favColour) {
     }
   );
 
-  console.log(htmlString);
+  fs.writeFileSync(`./_${(new Date).getTime()}.html`, htmlString);
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  page.setViewport(
+    {
+      width: 2480,
+      height: 3508
+    }
+  );
+  await page.setContent(htmlString);
+  await page.pdf({path: `./_${(new Date).getTime()}.pdf`, format: 'A4'});
+  
+  await browser.close();
+
+  // console.log(htmlString);
 }
 
 module.exports = renderHTML;
